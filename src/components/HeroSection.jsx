496a6e1294b0cc66d7905574495a9ead42b94e0b
shilviadiva <shilviadiva@gmail.com>
2025-09-11 12:39:49 +0700

    Commit file HeroSection

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+export default function HeroSection () {
+    return (
+        <>
+        <h3><u>Apa itu ReFood?</u></h3>
+        <p className="keterangan">
+            Website yang berisi ulasan atau review jenis-jenis kuliner, seperti 
+            <strong>makanan, minuman, ataupun snack</strong> dari berbagai restoran.
+            <br/>Pengguna juga dapat melihat review dari pengguna lainnya. 
+            Website ini bertujuan untuk memudahkan pengguna yang ingin 
+            <u>mencari referensi makanan</u>.
+        </p>
+        </>
+    )
+}
