54d464c12d378d360a64315e8b02ae918f8957e5
shilviadiva <shilviadiva@gmail.com>
2025-09-11 12:31:57 +0700

    Commit file Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>ReFood</h1>        
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="ulasan.html">Ulasan</a>
+            <a href="about.html">Tentang Kami</a>
+        </nav>
+    </div>
+  )
+}
