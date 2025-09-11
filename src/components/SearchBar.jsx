b9fd3614c695f2cdc4d9c3e5bde16ae89b7d83aa
shilviadiva <shilviadiva@gmail.com>
2025-09-11 12:35:27 +0700

    Commit file SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar () {
+    return (
+        <form className="search">
+            <input type="search" name="q" placeholder="Cari sesuatu..."/>
+            <button type="submit">Cari</button>
+        </form>
+    )
+}
