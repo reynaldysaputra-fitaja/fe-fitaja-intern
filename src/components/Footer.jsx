20b937c2e3d30b4637b96a7518234eb63e58c6f0
shilviadiva <shilviadiva@gmail.com>
2025-09-15 09:10:41 +0700

    Untuk footer yang berisi copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+export default function Footer() {
+    return(
+        <div className='footer'>
+            &copy; 2025 <b>Shilvia Diva Pertiwi.</b> All Rights Reserved.
+        </div>
+    )
+}
