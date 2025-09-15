ab0679009ff5a375af8eda8a72454083de9577e3
shilviadiva <shilviadiva@gmail.com>
2025-09-15 09:05:05 +0700

    Untuk komponen Card dengan menerima props

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+export default function Card({image, alt, title, desc, category}) {
+    return(
+        <div className='review'>
+            <img src={image} alt={alt}/>
+            <h5>{category}</h5>
+            <span>{title}</span>
+            <p>{desc}</p>
+        </div>
+    )
+}
