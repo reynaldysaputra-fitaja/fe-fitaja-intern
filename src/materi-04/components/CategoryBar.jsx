b0cab37fe3936ab567f41452186df1c92f13ca36
shilviadiva <shilviadiva@gmail.com>
2025-09-23 09:16:31 +0700

    Menambahkan state active untuk button category

--- src/materi-04/components/CategoryBar.jsx
+++ src/materi-04/components/CategoryBar.jsx
@@ -1,18 +1,30 @@
 import '../App.css'
+import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { setFilter } from "./todoSlice"
 
 export default function CategoryBar() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
+  const [active, setActive] = useState("all")
+
+  const categories = ["all", "work", "personal", "finance", "shopping", "study"]
+
+  const handleClick = (category) => {
+    setActive(category)
+    dispatch(setFilter(category))
+  }
     
   return (
     <div className="cat-bar">
-        <button onClick={() => dispatch(setFilter("all"))}>All</button>
-        <button onClick={() => dispatch(setFilter("work"))}>Work</button>
-        <button onClick={() => dispatch(setFilter("personal"))}>Personal</button>
-        <button onClick={() => dispatch(setFilter("finance"))}>Finance</button>
-        <button onClick={() => dispatch(setFilter("shopping"))}>Shopping</button>
-        <button onClick={() => dispatch(setFilter("study"))}>Study</button>
+        {categories.map((cat) => (
+        <button
+          key={cat}
+          className={active === cat ? "active" : ""}
+          onClick={() => handleClick(cat)}
+        >
+          {cat.charAt(0).toUpperCase() + cat.slice(1)}
+        </button>
+      ))}
     </div>
   )
 }
