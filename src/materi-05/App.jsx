eebaf024b5c668c3f0f592548c4a01637bc4c960
shilviadiva <shilviadiva@gmail.com>
2025-09-25 15:21:19 +0700

    Menambahkan app untuk memanggil komponen yang ada

---
+++ src/materi-05/App.jsx
@@ -0,0 +1,15 @@
+import SearchBar from "./components/SearchBar"
+import Header from "./components/Header"
+import Card from "./components/Card"
+
+function Materi05() {
+  return (
+    <div className="bg-white font-sans">
+        <SearchBar />
+        <Header />
+        <Card />
+    </div>
+  )
+}
+
+export default Materi05
