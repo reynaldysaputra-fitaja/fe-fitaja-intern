2977c19a6bef35acbbc1f7cb1eb8ed4887e07d1f
shilviadiva <shilviadiva@gmail.com>
2025-09-26 11:12:34 +0700

    Menambahkan komponen Add Product dan showAddForm

--- src/materi-05/App.jsx
+++ src/materi-05/App.jsx
@@ -1,11 +1,16 @@
 import SearchBar from "./components/SearchBar"
 import Header from "./components/Header"
 import Card from "./components/Card"
+import { useSelector } from "react-redux"
+import AddProduct from "./components/AddProduct"
 
 function Materi05() {
+  const showForm = useSelector(state => state.ui.showAddForm)
+
   return (
     <div className="bg-white font-sans">
         <SearchBar />
+        {showForm && <AddProduct/>}
         <Header />
         <Card />
     </div>
