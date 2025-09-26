df9c95156dd6a7ab124993608db59843508e03d2
shilviadiva <shilviadiva@gmail.com>
2025-09-26 10:58:33 +0700

    Menambahkan reducer dari UI Slice

--- src/materi-05/components/store.js
+++ src/materi-05/components/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import appReducer from "./appSlice"
+import uiReducer from "./uiSlice"
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
+    ui: uiReducer,
   },
 })
