70e9e3644a6105f1012e84b12d353ce3d3bd1951
shilviadiva <shilviadiva@gmail.com>
2025-09-29 15:12:44 +0700

    Menambahkan api reducer dari api.js

---
+++ src/materi-05/state/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit"
+import appReducer from "./appSlice"
+import uiReducer from "./uiSlice"
+import { api } from './api';
+
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+    ui: uiReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+})
