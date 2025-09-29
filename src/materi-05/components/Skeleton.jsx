02a9479edc1e1db6d982703560c51bd27ac64d4c
shilviadiva <shilviadiva@gmail.com>
2025-09-29 15:20:44 +0700

    Menambahkan komponen skeleton ketika hit API dengan isLoading

---
+++ src/materi-05/components/Skeleton.jsx
@@ -0,0 +1,8 @@
+export default function Skeleton() {
+  return (
+    <div className="rounded p-4 space-y-2 animate-pulse flex flex-col items-center">
+      <div className="h-4 bg-gray-300 rounded w-3/4" />
+      <div className="h-4 bg-gray-300 rounded w-1/2" />
+    </div>
+  );
+}
