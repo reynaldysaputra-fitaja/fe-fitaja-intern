5f14360a5873cbd11f12846dd42db13d1a34c2fd
shilviadiva <shilviadiva@gmail.com>
2025-10-01 14:22:24 +0700

    Menghapus uiReducer karena digabung ke appReducer

--- src/materi-05/state/store.js
+++ src/materi-05/state/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import appReducer from "./appSlice"
-import uiReducer from "./uiSlice"
 import { api } from '../service/api';
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
-    ui: uiReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
