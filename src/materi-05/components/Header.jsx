2a3c3ec9b3acce16763acfe04b9953bbeb2c333e
shilviadiva <shilviadiva@gmail.com>
2025-10-01 14:28:52 +0700

    Mengubah tampilan header jika tidak ada product

--- src/materi-05/components/Header.jsx
+++ src/materi-05/components/Header.jsx
@@ -1,24 +1,22 @@
 import { useGetPostsQuery } from "../service/api";
-import Skeleton from "./Skeleton";
 
 export default function Header() {
-  const { isLoading } = useGetPostsQuery();
-    
-    if (isLoading) {
-      return (
-        <div className="space-y-4 p-6">
-          <Skeleton />
-        </div>
-      );
-    }
+  const { data } = useGetPostsQuery();
+  const products = data || [];
   return (
-    <div className="mx-auto mt-10 md:mt-15">
-      <p className="text-center text-gray-500 text-base md:text-base lg:text-lg">
-        LAST UPDATED ON OCTOBER 2022
-      </p>
-      <p className="text-center font-bold text-3xl md:text-4xl lg:text-5xl mb-10 lg:mb-15">
-        10 best really cheap <br/> gaming laptops 
-      </p>
+    <div>
+      {products.length === 0 ? (
+        <div/>) : (
+          <div className="mx-auto mt-10 md:mt-15">
+          <p className="text-center text-gray-500 text-base md:text-base lg:text-lg">
+            LAST UPDATED ON OCTOBER 2022
+          </p>
+          <p className="text-center font-bold text-3xl md:text-4xl lg:text-5xl mb-10 lg:mb-15">
+            10 best really cheap <br/> gaming laptops 
+          </p>
+        </div>
+      )}
     </div>
+    
   )
 }
