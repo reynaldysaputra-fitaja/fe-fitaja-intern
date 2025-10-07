8b948db557aa38fe75fe3b08af82dbcb60fd9ca3
shilviadiva <shilviadiva@gmail.com>
2025-10-07 08:58:38 +0700

    Page ketika tidak ada id product yang dicari oleh user

---
+++ src/materi-05/components/PageNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div className="text-center font-bold">
+        <h6>404 Error</h6>
+        <p>Oops! The page you're looking for does not exist.</p>
+    </div>
+  )
+}
